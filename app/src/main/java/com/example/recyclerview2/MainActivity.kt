a449bf1a6832f98b4eae9480f0369d6533b6c352
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-07-05 22:05:58 +0530

    Logic

--- app/src/main/java/com/example/recyclerview2/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview2/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.recyclerview2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var blogAdapter: BlogRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet() {
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView() {
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val topSpacingDecoration = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecoration)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
+//        recycler_view.layoutManager = LinearLayoutManager(this@MainActivity)
+//        blogAdapter = BlogRecyclerAdapter()
+//        recycler_view.adapter = blogAdapter
     }
 }
